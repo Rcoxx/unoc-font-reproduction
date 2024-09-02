3f224be8eee831719f52d7ff7f740db4cf57716a
Doxo <gentleDone@outlook.com>
2024-09-02 13:34:28 +0800

    chore: init

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>UnoCSS Instant On-demand <br> Atomic CSS Engine</div>
+</template>
+
+<style>
+@import url('./styles/fonts.css');
+
+body {
+  font-family: 'aliph';
+  font-size: 30px;
+}
+</style>
